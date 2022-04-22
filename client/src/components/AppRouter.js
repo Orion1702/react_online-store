961fac36a62075e65a4222a571e635a778617470
AlexTa <alex.tarasov.work@gmail.com>
2022-04-22 19:38:27 +0300

    Add routs to switcher

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {Switch, Route, Redirect} from 'react-router-dom';
+import { authRoutes, publickRoutes } from "../routes";
+import { NOTFOUND_ROUTE } from "../utils/consts";
 
 const AppRouter = () => {
+    const isAuth = true;
     return(
         <Switch>
-            
+            {isAuth &&  authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            {publickRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            <Redirect to={NOTFOUND_ROUTE}/>
         </Switch>
     );
 };
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,10 @@
+import page404 from "./pages/404"
 import Admin from "./pages/Admin"
 import Auth from "./pages/Auth"
 import Basket from "./pages/Basket"
 import DevicePage from "./pages/DevicePage"
 import Shop from "./pages/Shop"
-import { ADMIN_ROUTE } from "./utils/consts"
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, NOTFOUND_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts"
 
 export const authRoutes = [
     {
@@ -32,5 +33,9 @@ export const publickRoutes = [
     {
         path: DEVICE_ROUTE + '/:id',
         Component: DevicePage
+    },
+    {
+        path: NOTFOUND_ROUTE,
+        Component: page404
     }
 ]
